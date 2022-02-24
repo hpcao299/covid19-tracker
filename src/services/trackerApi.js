a00037db462ef61fb945576ff8de237b83450308
HPhucJ2007 <caohoangphuc2909@gmail.com>
2022-02-24 21:02:35 +0700

    fix import path

--- src/services/trackerApi.js
+++ src/services/trackerApi.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseUrl = 'https://api.zingnews.vn/public/v2/corona';
 
