cdbcbb13a387a8a67e32a709b606b834d9b25a56
HPhucJ2007 <caohoangphuc2909@gmail.com>
2022-02-26 11:38:22 +0700

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center text-sm h-28">
             <p>Powered by Jack</p>
-            <span className="font-bold">Nguồn dữ liệu từ Zing News</span>
+            <span className="font-bold">Nguồn dữ liệu từ Zing News & VnExpress</span>
         </footer>
     );
 }
